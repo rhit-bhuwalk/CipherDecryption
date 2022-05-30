f6d38fc859922e5f1f4928293acd02d7d417343d
rhit-bhuwalk <bhuwalk@rose-hulman.edu>
2022-05-30 06:07:43 -0400

    cli made

--- src/InverseFinder.java
+++ src/InverseFinder.java
@@ -2,13 +2,18 @@
 public class InverseFinder {
 	
 	public static void inverse(int a, int m) {
-		
+		boolean found = false;
 		for(int i=1;i<m;i++) {
 			
-			if((a*i)%m == 1)
-				System.out.println(i);
+			if((a*i)%m == 1) {
+				found = true;
+				System.out.println("Multiplicative Inverse of "+ a + " (mod " + m + ") " + "is " + i);
+				
+			}
+			
 		}
-		System.out.println("not found");
+		if(!found)
+			System.out.println("Multiplicative inverse not found");
 		
 	}
 	
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,37 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		//String s = "FZWUAHZWJZWJWAKSUSWKWJUAHZWJSDKGCFGOFSKSKZAXLUAHZWJLZWFWPLAKSFSXXAFWUAHZWJJ";
-		//s = s.toLowerCase();
-		//ShiftCipher c = new ShiftCipher(s);
-		InverseFinder.inverse(1161, 26);
-		//ShiftCipher d = new ShiftCipher("QGJNJNLYQXNQVXNNLDXABKBTQRSLNNQBUXRJEGXK");
+		Scanner s = new Scanner(System.in);
+			
+			System.out.println("Welcome to the classical cipher decryption API.");
+			System.out.println("Enter I if you want to find the modular multiplicative inverse of two numbers");
+			System.out.println("Enter S if you want to decode a message using a shift cipher");
+			System.out.println("Enter A if you want to encrypt a message using an AES cipher");
+			String input = s.nextLine();
+			
+			
+			if(input.equals("I")) {
+				System.out.println("Enter 1st number");
+				int num = s.nextInt();
+				System.out.println("Enter mod");
+				int mod = s.nextInt();
+				InverseFinder i = new InverseFinder();
+				i.inverse(num, mod);
+				System.out.println("Thank you!");
+			}
+		
+		else if(input.equals("S")) {
+				System.out.println("Enter message");
+				String message = s.next();
+				ShiftCipher c = new ShiftCipher(message);
+				System.out.println("Thank you");
+		}
+			
+		else 
+				System.out.println("Wrong input, please try again");
+		
+		}
 	}
-}
+
